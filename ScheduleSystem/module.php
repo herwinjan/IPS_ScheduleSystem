dc225f98794803fb4e83a831e511e626c93d961d
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2017-09-17 15:53:17 +0200

    Form config script

--- ScheduleSystem/module.php
+++ ScheduleSystem/module.php
@@ -26,6 +26,104 @@ if (@constant('IPS_BASE') == null) { //Nur wenn Konstanten noch nicht bekannt si
  * @package VisonicGateway
  * @property int $Parent
  */
+
+Class BuienradarClass
+{
+    public $latitude=51.919;
+    public $longitude=4.266;
+
+
+
+    private $regendata=array();
+
+    public function __construct() {
+
+        $this->getBuienradarData();
+    }
+
+    public function getBuienradarData()
+    {
+        $url="http://gadgets.buienradar.nl/data/raintext?lat=".$this->latitude."&lon=".$this->longitude;
+        //print($url);
+        $data=@file_get_contents($url);
+        if ($data)
+        {
+            //print_r($data);
+            $rows=explode("\n",$data);
+            $regen=array();
+            foreach($rows as $r)
+            {
+                $dd=explode("|",$r);
+                if (strlen($dd[0])>0)
+                {
+                    $amount=round(pow(10, ($dd[0] - 109) / 32) ,2);
+
+                    //print(date("H:i",strtotime($dd[1]))."=".$amount."|");
+                    $regen[strtotime($dd[1])]=$amount;
+                }
+            }
+            $this->regendata=$regen;
+        }
+        return false;
+    }
+
+    public function getWolkenData()
+    {
+        $url="http://api.openweathermap.org/data/2.5/weather?id=2751285&units=metric&appid=e0b832ae5bb9e8922c3fbd5f8477a1a1";
+
+        //print($url);
+        $da=@file_get_contents($url);
+        if ($da)
+        {
+            $data=json_decode($da,true);
+
+            return $data["clouds"]["all"];
+            //print_r($data);
+
+
+        }
+        return false;
+    }
+
+    public function nowRain()
+    {
+        if (count($this->regendata)>0)
+        {
+            reset($this->regendata);
+            if (current($this->regendata)>0 || next($this->regendata)>0)
+                return true;
+        }
+        return false;
+    }
+    public function nowRainRate()
+    {
+        if (count($this->regendata)>0)
+        {
+            reset($this->regendata);
+            $r1=current($this->regendata);
+            $r2=next($this->regendata);
+            return round(($r1+$r2)/2,1);
+        }
+        return 0;
+    }
+    public function nextHourRainRate()
+    {
+        if (count($this->regendata)>0)
+        {
+            reset($this->regendata);
+            $sum=0;
+            for ($t=0; $t<12; $t++)
+            {
+                $sum+=current($this->regendata);
+                next($this->regendata);
+            }
+            return round($sum/12,1);
+        }
+        return 0;
+    }
+};
+
+
 class ScheduleSystem extends IPSModule
 {
     private $scriptConfig = "";
@@ -89,9 +187,117 @@ class ScheduleSystem extends IPSModule
         }
     }
 
+    public  function SetNewValue($dev, $status)
+    {
+        $prev=GetValue($dev);
+        if ($prev!=$status)
+        {
+            SetValue($dev,$status);
+        }
+    }
+    public  function getRandom()
+    {
+
+        return RegVar_GetBuffer(16062 /*[Maassluis\Alles\Today Random]*/);
+    }
+    public  function getTodaySunrise()
+    {
+        $sunriseBasis=GetValue(10831 /*[Location\Sunrise]*/);
+        return strtotime(sprintf("%02d:%02d",date("H",$sunriseBasis),date("i",$sunriseBasis)));
+    }
+    public  function getTodaySunset()
+    {
+        $sunsetBasis=GetValue(45900 /*[Location\Sunset]*/);
+        return strtotime(sprintf("%02d:%02d",date("H",$sunsetBasis),date("i",$sunsetBasis)));
+    }
+    public  function SetValue($dev, $status)
+    {
+        $ret=false;
+        $type = IPS_GetInstance($dev)["ModuleInfo"]["ModuleName"];
+        switch($type)
+        {
+            case "Z-Wave Module":
+                $ret=ZW_DimSet($dev,$status);
+                break;
+            case "Dummy Module": //Plugwise?
+                IPSUtils_Include("Plugwise_Include.ips.php","IPSLibrary::app::hardware::Plugwise");
+                IPSUtils_Include("Plugwise_Configuration.inc.php","IPSLibrary::config::hardware::Plugwise");
+                $ident=IPS_GetObject($dev)["ObjectIdent"];
+
+                if (isset($ident))
+                {
+                    $id=IPS_GetObjectIDByName("Status",$dev);
+                    $prev=GetValue($id);
+                    if ($prev!=$status)
+                    {
+                        $ret=circle_on_off($ident,$status);
+                        SetValue(IPS_GetObjectIDByName("Status",$dev),$status);
+                    }
+                }
+
+                break;
+        }
+        return $ret;
+    }
+
+    public function activateScene($scene)
+    {
+        foreach ($this->devices as $key=>$devices)
+        {
+            if ($key==$scene)
+            {
+                foreach ($devices as $dev)
+                {
+                    $this->SetNewValue($dev[0],$dev[1]);
+                }
+            }
+        }
+
+    }
+
     public function updateEvent()
     {
         IPS_LogMessage("SCHEDULER DEBUG", "check actions! #actions: ".count($this->actions));
+
+        foreach ($this->actions as $actionamem => $actionstr)
+        {
+            $dec=explode(":",$actionstr);
+            switch ($dec[0])
+            {
+                case "MOTION":
+                    $m=GetValue($dec[1]);
+                    if ($m>0)
+                    {
+                        IPS_LogMessage("SCHEDULER DEBUG", "Activate Motion Scene: $key");
+                        $this->activateScene($dec[2]);
+                    }
+
+                    break;
+                case "NONOTION":
+                    $time=strtotime("-5 minutes");
+                    $ar=AC_GetAggregatedValues ( 19483 /*[Archive]*/, $dec[1],$dec[2],$time,time(),0);
+                    $move=false;
+                    if (count($ar)>0)
+                    {
+                        foreach ($ar as $a)
+                        {
+                            if ($a["Max"])
+                            {
+                                $move=true;
+                            }
+                        }
+                    }
+                    $st=GetValue($dec[1]);
+                    if ($move==false && $st==true) {
+                        $this->activateScene($dec[3]);
+                        IPS_LogMessage("SCHEDULER DEBUG", "deActivate Motion Scene (".$dec[2].": $key");
+
+                    }
+
+                    break;
+            }
+
+        }
 
     }
 
